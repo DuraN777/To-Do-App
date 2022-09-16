cad91070fe3bdd0d3256a1c56ea67764191d1517
Djura Neskovic <neskovic991@gmail.com>
2022-09-16 13:24:26 +0200

    show todo-item on submit

--- app.js
+++ app.js
@@ -29,6 +29,66 @@ window.addEventListener('load', () => {
     todos.push(todo);
     localStorage.setItem('todos', JSON.stringify(todos));
     e.target.reset();
+
+    displayTodos();
   })
 
+  displayTodos();
+
 })
+
+function displayTodos() {
+  const todoList = document.querySelector('#todo-list');
+  todoList.innerHTML = '';
+  
+  todos.forEach(todo => {
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todo-item');
+
+    // Create HTML elements for todo-item
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+    const content = document.createElement('div');
+    const actions = document.createElement('div');
+    const edit = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+
+    input.type = 'checkbox';
+    // to check if todo is checked
+    // todo is our object and done is boolean property in it.
+    input.checked = todo.done;
+    span.classList.add('bubble');
+
+    // Check for category of todo
+    if (todo.category == 'personal') {
+      span.classList.add('personal');
+    } else {
+      span.classList.add('business');
+    }
+
+    // Adding styles to elements
+    content.classList.add('todo-content');
+    actions.classList.add('actions');
+    edit.classList.add('edit');
+    deleteBtn.classList.add('delete');
+
+    // INput element representing todo task
+    content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+    edit.innerHTML = 'Edit';
+    deleteBtn.innerHTML = 'Delete';
+
+    // Apending elements
+    label.appendChild(input);
+    label.appendChild(span);
+    actions.appendChild(edit);
+    actions.appendChild(deleteBtn);
+
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+
+    todoList.appendChild(todoItem);
+
+  });
+}
