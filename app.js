1cac413b4a89fd5ebb275bdec075f4ad7ea3dbd9
Djura Neskovic <neskovic991@gmail.com>
2022-09-16 12:54:22 +0200

    create and store todo object on submit

--- app.js
+++ app.js
@@ -13,4 +13,21 @@ window.addEventListener('load', () => {
     localStorage.setItem('username', e.target.value);
   })
 
+  newTodoForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Create todo object
+    const todo = {
+      // name pertains to name of element in new-todo-form
+      // content & category are names of elements which value we want to store
+      content: e.target.elements.content.value, 
+      category: e.target.elements.category.value,
+      done: false,
+      createdAt: new Date().toDateString(),
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  })
+
 })
